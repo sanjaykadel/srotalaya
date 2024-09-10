0574bed796be3540c602f7e94f1fc5b42e7b7d73
sanjay <kadelsanjay05@gmail.com>
2024-09-10 11:11:08 +0545

    logo

--- core/core/settings.py
+++ core/core/settings.py
@@ -114,11 +114,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
